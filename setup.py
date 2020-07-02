ef6814bbc6cbbeeb47b201352cbce904e752151a
RJ Patawaran <rjpatawaran@me.com>
2020-07-02 14:31:30 +0800

    Add publish command to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
-try:
-    from setuptools import find_packages, setup, Command
-except ImportError:
-    from distutils.core import find_packages, setup, Command
+from setuptools import find_packages, setup, Command
 
-requires = ["pymongo", "marshmallow", "future"]
+# requires = ["pymongo", "marshmallow", "future", "s3pypi"]
+
+class PublishCommand(Command):
+    description = 'Publish package to the private PyPI repository.'
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        self.spawn(['s3pypi', '--bucket', 'entropypi'])
 
 setup(name="entropy",
       version="0.1",
       packages=find_packages(),
-    #   cmdclass={"test": PyTest},
       platforms=["any"],
-
-      install_requires = ["pymongo<=2.8", "marshmallow<=2.21.0", "six"],
+      url='http://entropyph.com',
+      install_requires = ["pymongo", "marshmallow<=2.21.0", "six", "future"],
       zip_safe=False,
       include_package_data=True,
-
       author="RJ Patawaran",
       author_email="rjpatawaran@me.com",
       keywords=["mongo", "mongodb", "pymongo", "orm"],
+      cmdclass={
+        'publish': PublishCommand,
+      },
 )
