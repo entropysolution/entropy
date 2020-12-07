580a8156c730eda1913988f0270b85c1e6a517e3
RJ Patawaran <rjpatawaran@me.com>
2020-12-07 15:55:22 +0800

    Add merge_in_place to dict_tools

--- common/dict_tools.py
+++ common/dict_tools.py
@@ -1,8 +1,13 @@
-import _pickle as pickle
+# from copy import deepcopy
+# import _pickle as pickle
+import ujson
 from collections import Mapping
+from common.jsonify import jsonify
+
 
 def deepcopy(d):
-	return pickle.loads(pickle.dumps(d, -1))
+	#return pickle.loads(pickle.dumps(d, -1))
+	return ujson.loads(ujson.dumps(d))
 
 def merge_dicts(base_dict, append_dict, overwrite=False):
 	"""
@@ -10,7 +15,7 @@ def merge_dicts(base_dict, append_dict, overwrite=False):
     overwrite True will merge in-place
     """
 	if not isinstance(base_dict, dict) or not isinstance(append_dict, dict):
-		return None
+		return jsonify({'success': False})
 	if not overwrite:
 		copy_dict = deepcopy(base_dict)
 		merge_recursion(copy_dict, append_dict)
@@ -18,6 +23,7 @@ def merge_dicts(base_dict, append_dict, overwrite=False):
 	if overwrite:
 		merge_recursion(base_dict, append_dict)
 		return base_dict
+
 
 def merge_recursion(d1, d2):
 	"""Takes two dicts as arguments. Returns merged dicts by combining deeper levels recursively"""
@@ -27,3 +33,18 @@ def merge_recursion(d1, d2):
 			merge_recursion(v1, v2)
 		else:
 			d1[k] = v2
+
+
+def merge_in_place(d1, d2):
+    """
+    Modifies d1 in-place to contain values from d2.  If any value
+    in d1 is a dictionary (or dict-like), *and* the corresponding
+    value in d2 is also a dictionary, then merge them in-place.
+    """
+    for k,v2 in d2.items():
+        v1 = d1.get(k) # returns None if v1 has no value for this key
+        if ( isinstance(v1, Mapping) and
+             isinstance(v2, Mapping) ):
+            merge_in_place(v1, v2)
+        else:
+            d1[k] = v2
