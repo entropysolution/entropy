fae96a006f12a769efdefec4647765f10da863af
RJ Patawaran <rjpatawaran@me.com>
2020-12-10 14:33:12 +0800

    Add methods from gridloc

--- common/time_utils.py
+++ common/time_utils.py
@@ -1,7 +1,7 @@
 from calendar import timegm
 from itertools import tee, zip_longest
-from datetime import datetime, timezone
 from pytz import timezone as ptimezone
+from datetime import datetime, timezone, timedelta
 
 def pairwise(iterable):
     a, b = tee(iterable)
@@ -31,3 +31,29 @@ def utcnow():
 def tolocaltime(dt, tz_region):
     tz = ptimezone(tz_region)
     return dt.astimezone(tz)
+
+def convert_sec_to_str(seconds):
+    negative = False
+    if seconds < 0:
+        negative = True
+        seconds = abs(seconds)
+    td = timedelta(seconds=seconds)
+    def plural(n):
+        if n>1: return 's'
+        return ''
+    if td == timedelta(0):
+        return '0 seconds'
+    out = []
+    y, d = divmod(td.days, 365)
+    if y: out.append('%d year' % y + plural(y))
+    m, d = divmod(d, 30)
+    if m: out.append('%d month' % m + plural(m))
+    if d: out.append('%d day' % d + plural(d))
+    h, s = divmod(td.seconds, 60*60)
+    if h: out.append('%d hour' % h + plural(h))
+    m, s = divmod(s, 60)
+    if m: out.append('%d minute' % m + plural(m))
+    if s: out.append('%d second' % s + plural(s))
+    if negative:
+        out.append('ago')
+    return ' '.join(out)
