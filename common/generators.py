23af2c218a5aba456991c51de335103c665b83dc
RJ Patawaran <rjpatawaran@me.com>
2021-01-05 13:52:21 +0800

    Remove hash functions from generators

--- common/generators.py
+++ common/generators.py
@@ -13,34 +13,34 @@ def generate_alphanum(length):
 		str.append(choice(chars))
 	return ''.join(str)
 
-def generate_hash(input_string, pbkdf2=True, salt_length=24, cost_factor=12000):
-	if pbkdf2:
-		if isinstance(input_string, str):
-			input_string = input_string.encode('utf-8')
-		while True:
-			try:
-				salt = generate_alphanum(salt_length)
-				_hash = crypt(input_string, salt, cost_factor)
-				break
-			except:
-				pass
-		return 'PBKDF2$%s$%s$%s' % (salt, cost_factor, _hash)
-	else:
-		return sha256('ent%sropy' % (input_string)).hexdigest()
+# def generate_hash(input_string, pbkdf2=True, salt_length=24, cost_factor=12000):
+# 	if pbkdf2:
+# 		if isinstance(input_string, str):
+# 			input_string = input_string.encode('utf-8')
+# 		while True:
+# 			try:
+# 				salt = generate_alphanum(salt_length)
+# 				_hash = crypt(input_string, salt, cost_factor)
+# 				break
+# 			except:
+# 				pass
+# 		return 'PBKDF2$%s$%s$%s' % (salt, cost_factor, _hash)
+# 	else:
+# 		return sha256('ent%sropy' % (input_string)).hexdigest()
 
-def check_hash(test_string, input_string, pbkdf2=True):
-	if pbkdf2:
-		if isinstance(input_string, str):
-			input_string = input_string.encode('utf-8')
-		try:
-			algo, salt, cost_factor, _hash = test_string.split('$', 3)
-		except:
-			return False
-		if _hash == crypt(input_string, _hash):
-			return True
-		return False
-	else:
-		return sha256('ent%sropy' % (input_string).hexdigest()) == test_string
+# def check_hash(test_string, input_string, pbkdf2=True):
+# 	if pbkdf2:
+# 		if isinstance(input_string, str):
+# 			input_string = input_string.encode('utf-8')
+# 		try:
+# 			algo, salt, cost_factor, _hash = test_string.split('$', 3)
+# 		except:
+# 			return False
+# 		if _hash == crypt(input_string, _hash):
+# 			return True
+# 		return False
+# 	else:
+# 		return sha256('ent%sropy' % (input_string).hexdigest()) == test_string
 
 def generate_random_password():
 	 str = []
